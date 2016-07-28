9344f5c84e757431c490862901eb0498ae731102
snowgoing <snowgoing@gmail.com>
2016-07-28 15:08:41 -0700

    Add Roster Barn Code

--- src_original/components/app.js
+++ src_original/components/app.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render () {
     return (
       <div>
-        <h1>Change me</h1>
+        <h1>Why Is this still connected?</h1>
       </div>
     )
   }
